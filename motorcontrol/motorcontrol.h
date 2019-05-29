455d93751b83737915f1e254e35a0f05ca71a075
poojitha-sai <51117529+poojitha-sai@users.noreply.github.com>
2019-05-29 21:17:18 +0200

    Create motorcontrol.h

---
+++ motorcontrol/motorcontrol.h
@@ -0,0 +1,11 @@
+//Parameters for sensor
+
+int sens_length = 0.17
+int sens_width = 0.03
+int sens_thickness = 0.0014
+
+//Parameters for stepper motor and setup
+
+int steps_per_turn = 200
+int turns_per_mm = 1 /*not sure*/
+int calib_default = -55
